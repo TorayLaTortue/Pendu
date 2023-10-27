8c06727ef7033b5d3c727bae8b79c476bf8451f6
PASQUER <damien.pasquer3636@gmail.com>
2023-10-27 16:33:56 +0200

    check lettre fonctionne

--- Main.c
+++ Main.c
@@ -4,8 +4,8 @@
 
 int main(void)
  {
-   char nom[20]; 
-   char mot[20];
+   char nom[10]; 
+   char mot[10];
    char lettre;
    int i = 0;
    int a = 0;
@@ -18,10 +18,15 @@ int main(void)
    int victoire = 2;//2 = partie en cours 1 = win 0 = lose 
    int lettreDejaUtilisee = 0;
 
-   printf("Entrez un Nom : ");
+
+   printf("Bienvenue dans le jeu du pendu !\n");
+   printf("Entrez un Nom (Max 10 charactere) : ");
    scanf("%s", nom);
    printf("Vous avez entré le nom : %s\n", nom);
    
+   
+
+
    for (i = 0; i < longueur; i++)// mettre des _ pour chaque lettre
    {
       tab[i] = '_';
@@ -32,47 +37,41 @@ int main(void)
    printf("\n");
    while (victoire == 2)
    {
+      printf("\n");
+      lettreDejaUtilisee = 0;
       scanf(" %c", &lettre);
+      o = 0;
       for(o = 0; o < a; o++)
       {
-         if (tab2[o] == lettre) 
+         if (tab2[o] == lettre) //empechez de pouvoir mettre deux fois la meme lettre
          {
             lettreDejaUtilisee = 1;
          }
-         else
-         {
-            lettreDejaUtilisee = 0;
-         }
       }
-      if (lettreDejaUtilisee = 0)
+      if (lettreDejaUtilisee == 0)//si la lettre a pas ete utiliser la rajouter a la liste
       {
          tab2[a] = lettre;
          a++;
       }
       
       
-
+      i = 0;
       for (i = 0; i < longueur; i++)// montrer le mot en _
    {
       printf("%c ", tab[i]);
 
    }
    printf("\n Lettre deja utilisé : \n");
+   k = 0;
 for (k = 0; k < a; k++)// mettre toutes les lettres utilisé
    {
       
       printf("%c , ", tab2[k]);
-
-      
-       
    }
    
 
    }
    
-   
-
-
    return 0;
 
  }
