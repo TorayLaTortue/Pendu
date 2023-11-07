973ff68f548bd6f369382127bbbcf39232864cfd
PASQUER <damien.pasquer3636@gmail.com>
2023-11-07 16:55:42 +0100

    ;-;

--- Main.c
+++ Main.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
+
+
+ #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
    char nom[10]; 
    char mot[10];
    char lettre;
+   char randomMot;
    int i = 0;
    int a = 0;
    int k = 0;
    int o = 0;
    int z = 0;
-   int e = 0;
-   strcpy(mot, "tortue"); // mot tortue
-   int longueur = strlen(mot);//longueur du mot
-   char tab[longueur]; // tableau de _
+   int j = 0;
+   int y = 0;
+   int MotTrouver = 0; //1 = mot trouve
    char tab2[26];//tableau de lettre utilisé*
    int lettreDejaUtilisee = 0;//detecter si la valeur a ete utiliser
 
@@ -24,31 +27,46 @@ int main(void)
    printf("Entrez un Nom (Max 10 charactere) : ");
    scanf("%s", nom);
    printf("Vous avez entré le nom : %s\n", nom);
-   
-   
 
+
+   srand(time(NULL)); //Choisir une valeur aléatoire par rapport a l'heure
+   int nombreAleatoire = (rand() % 835);
+   FILE *fichier = fopen("words.txt", "r");
+   char ligne[100]; // Longueur d'une ligne
+    int numeroLigne = 1; // Compteur pour le numéro de la ligne
+
+   while(fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        numeroLigne++;
+        if (numeroLigne == nombreAleatoire) {
+          printf("Ligne: %s", ligne);
+            break; // Sortir de la boucle une fois que la ligne aléatoire a été lue
+        }
+   }
+   strncpy(mot, ligne, 10);
+   ligne[strcspn(ligne, "\n")] = '\0';
+   mot[10] = '\0';
+
+   int longueur = strlen(mot) - 1;//longueur du mot
+   char tab[longueur]; // tableau de _
 
    for (i = 0; i < longueur; i++)// mettre des _ pour chaque lettre
    {
-      tab[i] = '_';
+      tab[i] = '_';     
       printf("%c ", tab[i]);
-       
    }
    i = 0;
 
-   while (a < 8)
+   while (y < 8)
    {
-
+      MotTrouver = 1;
       printf("\n");
       scanf(" %c", &lettre);
       printf("\n");
-
       while(!isalpha(lettre)) //verifier si c'est bien une lettre qui est entrée
       {
         printf("Ceci n'est pas une lettre.\n \n");
         scanf(" %c", &lettre);
       }
-
 
       o = 0;
       lettreDejaUtilisee = 0;
@@ -59,50 +77,62 @@ int main(void)
             lettreDejaUtilisee = 1;
          }
       }
+
       if (lettreDejaUtilisee == 0)//si la lettre a pas ete utiliser la rajouter a la liste
       {
          tab2[a] = lettre;
          a++;
-         
       }
       
       
       i = 0;
       z = 0;
-      for (i = 0; i <= longueur; i++)// montrer le mot en _
+      for (i = 0; i < longueur; i++)// montrer le mot en _
    {
 
       for(z = 0; z <= longueur; z++)//Remplacer les _ par les lettre trouver
       {
          if (tab2[z] == mot[i])
-      {
-         tab[i] = tab2[z];
-         
+         {
+            tab[i] = tab2[z];
+         }
       }
-      
-      }
-      printf("%c ", tab[i]);
 
-      e = 0;
+      printf("%c ", tab[i]);
+      
       if (tab[i] == '_')
       {
-         e = 1;
+         MotTrouver = 0;
       }
       
 
-      
    }
+   
+   i = 0;
+   
+   for (i = 0; i < longueur; i++)// nombre d'essaie
+   {
+         if (lettre == mot[i])
+         {
+            break;
+         }
+               
+   }
+   
 
    printf("\n Lettre deja utilisé : \n");
    k = 0;
    for (k = 0; k < a; k++)// mettre toutes les lettres utilisé
    {
-      
       printf("%c , ", tab2[k]);
    }
-
    
    
+   if (MotTrouver == 1)
+   {
+      printf("\n \n Bravo %s vous avez reussi en %d essai \n", nom, a);
+      break;
+   }
    
 
 
@@ -123,6 +153,11 @@ default:
    }
    }
    
-   printf("\n \n Finis ! \n");
+
+   if (MotTrouver == 0)
+   {
+      printf("\n \n Perdu le mot était : %s ! \n", mot);
+   }
+   
 
 }
