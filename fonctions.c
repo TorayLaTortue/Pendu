dd312c51680cbdb95dd744ee204efbd772c62d71
PASQUER <damien.pasquer3636@gmail.com>
2023-11-18 13:52:44 +0100

    fichier

---
+++ fonctions.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+
+const char getTexte(int y) {
+
+        switch (y) {
+            case 1:
+               printf(" __________\n");
+               printf("|__________|\n");
+            break;
+         
+            case 2:
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+         
+            case 3:
+               printf(" __________\n");
+               printf("|| /\n");
+               printf("||/\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+      
+            case 4:
+               printf(" __________\n");
+               printf("|| /      |\n");
+               printf("||/       |\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+
+            case 5:
+               printf(" __________\n");
+               printf("|| /      |\n");
+               printf("||/       |\n");
+               printf("||       ( )\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+
+            case 6:
+               printf(" __________\n");
+               printf("|| /      |\n");
+               printf("||/       |\n");
+               printf("||       ( )\n");
+               printf("||        |\n");
+               printf("||        |\n");
+               printf("||        |\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+
+            case 7:
+               printf("__________\n");
+               printf("|| /      |\n");
+               printf("||/       |\n");
+               printf("||       ( )\n");
+               printf("||        |\n");
+               printf("||       /|\\ \n");
+               printf("||        |\n");
+               printf("||\n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+
+            case 8:
+               printf(" __________\n");
+               printf("|| /      |\n");
+               printf("||/       |\n");
+               printf("||      (+_+)\n");
+               printf("||        |\n");
+               printf("||       /|\\ \n");
+               printf("||        |\n");
+               printf("||       / \\ \n");
+               printf("||\n");
+               printf("||_________\n");
+               printf("|__________|\n");
+            break;
+        }
+    }
